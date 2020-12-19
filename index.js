6ec24c1e3b2b9336d6f71f5aa778c97bef449f5d
aqmeraamir <59936217+aqmeraamir@users.noreply.github.com>
2020-12-19 13:58:22 -0800

    Website Test

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile("./index.php")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
 
 
 require("./Events/logger.js")(client)
